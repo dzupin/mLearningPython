d7cb6c56d7cd4932a1fc7000c5345a087d0576fb
dzupin <dzupinr@gmail.com>
2016-06-27 17:34:47 +0200

    Tensor Flow official tutorial code

--- tfMnistDataset.py
+++ tfMnistDataset.py
@@ -1,25 +1,37 @@
+#The downloaded MNIST database of handwritten digits, from http://yann.lecun.com/exdb/mnist/, is for our convenience split into three parts:
+# 55,000 data points of training data (mnist.train), 10,000 points of test data (mnist.test), and 5,000 points of validation data (mnist.validation).
 from tensorflow.examples.tutorials.mnist import input_data
 mnist = input_data.read_data_sets("MNIST_data/", one_hot=True)
 
+#Sample code to process sample mnist data
 import tensorflow as tf
+
+#Define your x(input) W(weight) and b(bias)
 x = tf.placeholder(tf.float32, [None, 784])
 W = tf.Variable(tf.zeros([784, 10]))
 b = tf.Variable(tf.zeros([10]))
 
+#implement our model
 y = tf.nn.softmax(tf.matmul(x, W) + b)
 
+#TRAINING SECTION
+#define true results
 y_ = tf.placeholder(tf.float32, [None, 10])
+#Evaluate how good/bad is our model by calculating cost (variability between known results y_ and our model result y)
 cross_entropy = tf.reduce_mean(-tf.reduce_sum(y_ * tf.log(y), reduction_indices=[1]))
+#Backpropagation algorithm that tries to minimize cost by optimizing variables in our model
 train_step = tf.train.GradientDescentOptimizer(0.5).minimize(cross_entropy)
 
 init = tf.initialize_all_variables()
 sess = tf.Session()
 sess.run(init)
 
+#Run our training cycle 1000 times
 for i in range(1000):
   batch_xs, batch_ys = mnist.train.next_batch(100)
   sess.run(train_step, feed_dict={x: batch_xs, y_: batch_ys})
 
+#Evaluate our model
 correct_prediction = tf.equal(tf.argmax(y,1), tf.argmax(y_,1))
 accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
 
